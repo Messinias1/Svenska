01f79b6b17b2e2a30cb432252ae6a3a56fb23896
Carl Kakisis <Messinias1@gmail.com>
2019-10-19 20:03:13 -0400

    axios testing

--- client/src/components/radio/Radio.js
+++ client/src/components/radio/Radio.js
@@ -1,11 +1,41 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Radio = () => {
-  return (
-    <Fragment>
-      <h1>Yo</h1>
-    </Fragment>
-  );
-};
+class Radio extends Component {
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://api.sr.se/api/v2/programcategories?format=json&indent=true")
+      .then(res => {
+        console.log(res);
+        this.setState({ categories: res.data.programcategories });
+      });
+  }
+  render() {
+    const { categories } = this.state;
+    const catList = categories.length ? (
+      categories.map(cat => {
+        return (
+          <div className="post card" key={cat.id}>
+            <div className="card-content">
+              <span className="card-title">{cat.name}</span>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No Categories</div>
+    );
+    return (
+      <div className="container">
+        <h1>Stations</h1>
+        <h3 className="center">{catList}</h3>
+      </div>
+    );
+  }
+}
 
 export default Radio;
