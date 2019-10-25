0d6bcf1adab211c8cb69c361fbdb6e28ab0e012a
Carl Kakisis <Messinias1@gmail.com>
2019-10-25 15:29:07 -0400

    added some front end fixes

--- client/src/components/radio/Radio.js
+++ client/src/components/radio/Radio.js
@@ -6,69 +6,61 @@ class Radio extends Component {
     super(props);
 
     this.state = {
-      channels: [],
-      page: 1
+      channels: []
     };
 
     this.nextPage = this.nextPage.bind(this);
-    this.startPage = this.startPage.bind(this);
   }
 
   nextPage() {
     axios
-      .get(
-        `http://api.sr.se/api/v2/channels/?format=json&indent=true&page=${this.state.page}`
-      )
+      .get("http://api.sr.se/api/v2/channels/?format=json&indent=true&size=60")
       .then(res => {
         console.log(res.data.channels);
         console.log(res.data.pagination);
-        this.setState({ page: this.state.page + 1 });
         this.setState({ channels: res.data.channels });
       });
-
-    console.log(this.state.page);
-  }
-
-  startPage() {
-    axios
-      .get("http://api.sr.se/api/v2/channels/?format=json&indent=true&page=1")
-      .then(res => {
-        console.log(res.data.pagination);
-        this.setState({ page: 2 });
-        this.setState({ channels: res.data.channels });
-      });
-    console.log(this.state.page);
   }
 
   componentDidMount() {
-    window.scrollTo(0, 0);
     this.nextPage();
-    this.startPage();
   }
 
   render() {
     const { channels } = this.state;
     const channelList = channels.length ? (
       channels.map(channel => {
+        var audio = new Audio(channel.liveaudio.url);
+
         return (
-          <Fragment key={channel.id}>
-            <div className="card">
-              <div className="card-content">
-                <h1 className="large text-primary">{channel.name}</h1>
-                <p className="m-lead">{channel.tagline}</p>
-                <img src={channel.image} alt="" id="channel-image" />
-                <p className="lead">
-                  {" "}
-                  <audio controls>
-                    <source src={channel.liveaudio.url} type="audio/mpeg" />
-                    <source src={channel.liveaudio.url} type="audio/ogg" />
-                    <source src={channel.liveaudio.url} type="audio/wav" />
-                  </audio>
-                  {/* <a href={channel.liveaudio.url}>Listen Now!</a> */}
-                </p>
-                <p className="lead">
-                  <a href={channel.siteurl}>Go to {channel.name}'s Website</a>
-                </p>
+          <Fragment className="row">
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <div class="overlay"></div>
+                  {/* <h1 className="large text-primary">{channel.name}</h1> */}
+                  <img
+                    src={channel.image}
+                    alt=""
+                    id="channel-image"
+                    onClick={() => audio.play()}
+                    onDoubleClick={() => audio.pause()}
+                  />
+
+                  {/* <p className="m-lead">{channel.tagline}</p> */}
+                  {/* <p className="lead">
+                    {" "}
+                    <audio controls id="aud">
+                      <source src={channel.liveaudio.url} type="audio/mpeg" />
+                      <source src={channel.liveaudio.url} type="audio/ogg" />
+                      <source src={channel.liveaudio.url} type="audio/wav" />
+                    </audio>
+                    <a href={channel.liveaudio.url}>Listen Now!</a>
+                  </p> */}
+                  <p className="m-lead">
+                    <a href={channel.siteurl}>Go to Site</a>
+                  </p>
+                </div>
               </div>
             </div>
           </Fragment>
@@ -80,7 +72,7 @@ class Radio extends Component {
       </div>
     );
     return (
-      <div className="container">
+      <div>
         <h1 className="large text-primary">Stations</h1>
         <p className="lead">
           <i className="fas fa-arrow-alt-circle-down">
@@ -88,39 +80,7 @@ class Radio extends Component {
             Learn Swedish By Listening to Radio Stations Below
           </i>
         </p>
-        <Fragment>
-          {this.state.page > 2 ? (
-            <div>
-              <button className="btn btn-primary" onClick={this.startPage}>
-                Start Page
-              </button>
-              <button className="btn btn-primary" onClick={this.nextPage}>
-                Next Page
-              </button>
-            </div>
-          ) : (
-            <button className="btn btn-primary" onClick={this.nextPage}>
-              Next Page
-            </button>
-          )}
-        </Fragment>
         <div>{channelList}</div>
-        <Fragment>
-          {this.state.page > 2 ? (
-            <div>
-              <button className="btn btn-primary" onClick={this.startPage}>
-                Start Page
-              </button>
-              <button className="btn btn-primary" onClick={this.nextPage}>
-                Next Page
-              </button>
-            </div>
-          ) : (
-            <button className="btn btn-primary" onClick={this.nextPage}>
-              Next Page
-            </button>
-          )}
-        </Fragment>
       </div>
     );
   }
